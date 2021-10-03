78cefa85f4555a24d23ed99f129aa5add7b6510c
Peter-Abayomi <89710410+Peter-Abayomi@users.noreply.github.com>
2021-10-03 07:55:23 +0100

    Update check_numbers.py

--- check_numbers.py
+++ check_numbers.py
@@ -6,6 +6,6 @@ def check_numbers(numbers):
             print('There are no even number in this list')
 
 
-values = [1, 5, 7, 5, 3]
+values = [2, 5, 4, 6, 8, 10, 9, 7, 3, 11, 12, 13, 14, 16, 15, 17, 18, 16]
 
 check_numbers(values)
