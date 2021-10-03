3151bade051380bc91eac3499a68a020e2515787
Peter-Abayomi <89710410+Peter-Abayomi@users.noreply.github.com>
2021-10-03 09:21:36 +0100

    Update temprature_converter.py

--- temprature_converter.py
+++ temprature_converter.py
@@ -1,10 +1,13 @@
-temperature = int(input("Input the temperature: "))
-unit = input("(C)Celsius or (F)Fahrenheit: ")
-print(type(temperature))
+try:
+    temperature = int(input("Input the temperature: "))
+    unit = input("(C)Celsius or (F)Fahrenheit: ")
 
-if unit.upper() == "C":
-    converted = (temperature * 9/5) + 32
-    print(f"The new temperature is {converted}Fahrenheit")
-else:
-    converted = (temperature - 32) * 5/9
-    print(f"The new temperature is {converted} Celsius")
+    if isinstance(temperature, int) and unit.upper() == "C":
+        converted = (temperature * 9/5) + 32
+        print(f"The new temperature is {converted}Fahrenheit")
+
+    elif isinstance(temperature, int) and unit.upper() == "F":
+        converted = (temperature - 32) * 5 / 9
+        print(f"The new temperature is {converted}Celsius")
+except ValueError:
+    print("You entered a string, please enter an integer")
