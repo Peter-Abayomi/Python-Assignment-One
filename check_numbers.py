9f52f8adf603ad84582b0aac2586469c4565fb56
Peter-Abayomi <89710410+Peter-Abayomi@users.noreply.github.com>
2021-10-05 01:15:31 +0100

    Update check_numbers.py

--- check_numbers.py
+++ check_numbers.py
@@ -1,15 +1,16 @@
 def check_numbers(number):
-    if bool(number) is False:
-        print("The list is empty")
-    elif bool(number) is True:
+    if number:
         for item in number:
             if isinstance(item, int) and item % 2 == 0:
                 print(item)
             elif isinstance(item, str):
                 print('There are no numbers in the list')
+    else:
+        print("The list is empty")
 
 
 number = [2, 5, 4, 6, 8, 10, 9, 7, 3, 11, 12, 13, 14, 16, 15, 17, 18, 16]
 
 check_numbers(number)
+
 
