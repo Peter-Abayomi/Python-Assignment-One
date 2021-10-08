248159c78c9a321b895bd618d3cff62645c79331
Peter-Abayomi <89710410+Peter-Abayomi@users.noreply.github.com>
2021-10-08 17:45:34 +0100

    Update check_numbers.py

--- check_numbers.py
+++ check_numbers.py
@@ -1,16 +1,18 @@
 def check_numbers(number):
-    if number:
-        for item in number:
-            if isinstance(item, int) and item % 2 == 0:
-                print(item)
-            elif isinstance(item, str):
-                print('There are no numbers in the list')
-    else:
-        print("The list is empty")
+    if isinstance(number, list) == 0:
+        return "This is not a list"
+    if bool(number) is False:
+        return "The list is empty"
+    for item in number:
+        if not isinstance(item, int):
+            return "List contains no numbers"
+    number_list = []
+    for item in number:
+        if item % 2 == 0:
+            number_list.append(item)
+    return('There are no even numbers' if bool(number_list) is False else number_list)
 
 
-number = [2, 5, 4, 6, 8, 10, 9, 7, 3, 11, 12, 13, 14, 16, 15, 17, 18, 16]
-
-check_numbers(number)
+list_of_number = [2, 3, 5, 7, 8, 4, 6, 12, 13, 14, 15, 16, 1, 17, 18, 20, 21]
 
 
